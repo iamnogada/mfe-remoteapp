96bd13dc0f427bbdd923e53f55f5bd59ac152e4d
nogada <seungil.han@gmail.com>
2024-03-13 11:10:24 +0900

    lint main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 
-from fastapi import FastAPI, APIRouter,Request, Response, Path
-from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.base import BaseHTTPMiddleware
 
-from app.utils import logger, RouterLoader, Template
+from app.utils import RouterLoader, Template
 from app.middlewares import MFEMiddleware
 
 
@@ -16,10 +15,10 @@ APP_NAME="/mfe"
 
 app = FastAPI(root_path=f"{APP_NAME}",title=f"{APP_NAME}", version="0.1.0",debug=True)
 
-app.mount(f"/assets", StaticFiles(directory="public/assets"), name="assets")
-app.mount(f"/js", StaticFiles(directory="public/js"), name="js")
-app.mount(f"/css", StaticFiles(directory="public/css"), name="css")
-app.mount(f"/test", StaticFiles(directory="public/css"), name="html")
+app.mount("/assets", StaticFiles(directory="public/assets"), name="assets")
+app.mount("/js", StaticFiles(directory="public/js"), name="js")
+app.mount("/css", StaticFiles(directory="public/css"), name="css")
+app.mount("/test", StaticFiles(directory="public/css"), name="html")
 
 RouterLoader(app)
 # If Reqeust form HTMX, then response only block html, othwerwise return full html including header
